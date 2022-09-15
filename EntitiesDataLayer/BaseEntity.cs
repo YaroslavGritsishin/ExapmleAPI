bb6e18ea9fb5264c5cfdbf71325c0e1d8ae5e228
Yaroslav Gritsishin <gritsishinyar@gmail.com>
2022-09-15 18:29:37 +0300

    Added entities of data layer

---
+++ EntitiesDataLayer/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using EntitiesDataLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiesDataLayer
+{
+    public class BaseEntity : IEntityBase
+    {
+        [Key]
+        public int Id { get; set ; }
+    }
+}
---
+++ EntitiesDataLayer/CategoryEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiesDataLayer
+{
+    public class CategoryEntity: BaseEntity
+    {
+        public CategoryEntity()
+        {
+            Products = new List<ProductEntity>();
+        }
+        public string CategoryName { get; set; }
+        public ICollection<ProductEntity> Products { get; set; }
+    }
+}
